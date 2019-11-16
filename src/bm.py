94c42e1357107de5f6f52632a579aab0bab803f4
eslam.gamal <slom72@gmail.com>
2019-11-16 11:59:44 +0200

    Implement BM Pattern Search Algorithem

--- src/bm.py
+++ src/bm.py
@@ -3,7 +3,8 @@ from typing import Dict
 
 def make_km_table(pattern: str) -> Dict[str, int]:
     table = dict()
-    raise Exception("TODO")
+    for index, char in reversed(list(enumerate(pattern))):
+        table[char] = index
     return table
 
 
@@ -15,9 +16,21 @@ class Bm(object):
 
     def decide_slide_width(self, c: str) -> int:
         assert len(c) == 1
-        raise Exception("TODO")
-        return -1
+        return self.table.get(c, -1)
 
     def search(self) -> int:
-        raise Exception("TODO")
+        current_index = len(self.pattern) - 1
+        slide_width = 0
+        while slide_width < (len(self.text) - len(self.pattern) + 1):
+            current_index = len(self.pattern) - 1
+
+            while current_index >= 0 and self.pattern[
+                    current_index] == self.text[slide_width + current_index]:
+                current_index -= 1
+
+            if current_index < 0:
+                return slide_width
+            else:
+                slide_width += max(1, current_index - self.decide_slide_width(
+                    self.text[slide_width + current_index]))
         return -1
